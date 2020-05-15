ceb2abd9afe567be6d0edee0cdcc68fddf4e80c6
AntonioSE <antonio.salazar@oliverwyman.com>
2020-05-14 20:59:28 -0500

    added some subfolders to the components folder, added the routes in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+//components
+import Login from './components/auth/Login';
+import NewAccount from './components/auth/NewAccount';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
-    <Fragment>
-      
-    </Fragment>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Login}/>
+        <Route exact path='/new-account' component={NewAccount}/>
+        <Route exact path='/projects' component={Projects}/>
+      </Switch>
+    </Router>
   );
 }
 
